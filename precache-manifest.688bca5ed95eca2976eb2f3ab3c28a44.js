a9aaed52bf835b557e59b23cb1a65c3c45468d1f
Alvin Nguyen <alvinnguyen116@gmail.com>
2018-12-21 03:26:17 -0800

    Updates

---
+++ precache-manifest.688bca5ed95eca2976eb2f3ab3c28a44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca6212a0648b1ec2c76b",
+    "url": "/2048/static/js/runtime~main.ca6212a0.js"
+  },
+  {
+    "revision": "25a6d8253c16b641d404",
+    "url": "/2048/static/js/main.25a6d825.chunk.js"
+  },
+  {
+    "revision": "f00f7917422a4e36b40b",
+    "url": "/2048/static/js/1.f00f7917.chunk.js"
+  },
+  {
+    "revision": "25a6d8253c16b641d404",
+    "url": "/2048/static/css/main.6a54058d.chunk.css"
+  },
+  {
+    "revision": "5f046898d3dee512eaa79040bd71e1a2",
+    "url": "/2048/index.html"
+  }
+];
